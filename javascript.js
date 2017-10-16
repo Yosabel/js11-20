93f718fbc462785d01712c15a42df881d31e9383
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2017-10-16 12:18:52 -0700

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+// 1. firstLast6
+// Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be
+// length 1 or more.
+//
+// firstLast6([1, 2, 6]) → true
+// firstLast6([6, 1, 2, 3]) → true
+// firstLast6([13, 6, 1, 2, 3]) → false
+
+function firstLast6([]){
+if((x[0]==6)){
+    return true;
+}
+if(x[x.length]==6]{
+    return true;
+} else{
+    return false;
+}
+}
+function tester() {
+
+    document.getElementById("output").innerHTML = firstLast6([6,5,8]);
+    // document.getElementById("output2").innerHTML = nextOne(true, false);
+    //test third method, etc
+}
